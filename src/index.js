342c4649e728714da4ef744e4ec37910b9e8d64b
drax3l <alexjeanpierquispesierra@gmail.com>
2025-02-02 18:07:11 -0500

    realizar configuracion 2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import express from 'express'
 import { dirname, join } from 'path'
 import { fileURLToPath } from 'url'
-
+import router from './routes/index.js'
 const app= express()
 const __dirmname= dirname(fileURLToPath(import.meta.url))
 console.log(join(__dirmname,'views'))
 
 app.set('views', join(__dirmname,'views'))
 app.set('view engine', 'ejs')
-
-app.get('/', (req, res) => res.render('index'))
+app.use(router)
+app.use(express.static(join(__dirmname,'public')))
 
 app.listen(3000)
 console.log('el server esta siendo escuchado en el puerto 3000')
